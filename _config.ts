6e3d60cc0c94e5a41b34a2682a91d1a376e3d6b4
Rick Cogley <rick.cogley@esolia.co.jp>
2024-07-02 08:23:25 +0900

    Revert cache busting

--- _config.ts
+++ _config.ts
@@ -15,7 +15,7 @@ import pagefind from "lume/plugins/pagefind.ts";
 import { getGitDate } from "lume/core/utils/date.ts";
 import sourceMaps from "lume/plugins/source_maps.ts";
 
-import cache_busting from "https://raw.githubusercontent.com/lumeland/experimental-plugins/c8778bfbf480f57a2357ab94bc22290b8bf11d12/cache_busting/mod.ts";
+// import cache_busting from "https://raw.githubusercontent.com/lumeland/experimental-plugins/c8778bfbf480f57a2357ab94bc22290b8bf11d12/cache_busting/mod.ts";
 
 // import en from "npm:date-fns@2.30.0/locale/en-US/index.js";
 // import ja from "npm:date-fns@2.30.0/locale/ja/index.js";
@@ -64,7 +64,7 @@ site.use(filterPages({
 // Use the source maps plugin to generate the .map files
 site.use(sourceMaps());
 
-site.use(cache_busting());
+// site.use(cache_busting());
 
 site.preprocess([".html"], (pages) => {
   for (const page of pages) {
