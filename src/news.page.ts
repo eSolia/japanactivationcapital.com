755d5604185ae75cd2150ad3240712fb0f8eaaef
Rick Cogley <rick.cogley@esolia.co.jp>
2024-11-03 19:25:37 +0900

    limit paginator size to 5 from default 10

--- src/news.page.ts
+++ src/news.page.ts
@@ -24,6 +24,7 @@ export default function* ({ search, paginate }: Lume.Data) {
         data.id = `news_list_${n}`;
         data.lang = lang;
       },
+      size: 5, // Default paginator size is 10
     });
 
     for (const page of result) {
